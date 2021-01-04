af2a3e9ae6786815c6eaf567514d540614738f9c
qlemql <busgamer@naver.com>
2021-01-04 16:48:24 +0900

    State
    setState를 호출해야 react는 다시 render를 한다
    
    this.setState({count: this.state.count - 1});
    =>
    this.setState(current => ({count: current.count + 1}));
    
    current로 현재 state를 가져올 수 있다.

--- src/App.js
+++ src/App.js
@@ -3,44 +3,24 @@ import PropTypes from "prop-types";
 
 
 
-const KindOfAnimals = [
-  {
-    id:1,
-    name : "penguin",
-    image:"https://c.files.bbci.co.uk/CF3C/production/_111925035_penguino.jpg"
-  },
-  {
-    id:2,
-    name : "cat",
-    image:"https://img.kyodonews.net/english/public/images/posts/999dc7ea8c73f32baa5361320e39c19a/photo_l.jpg"
+class App extends React.Component{
+  state = {
+    count: 0
   }
-]
-
-function Animals({ name, picture }){
-  return (
-    <div>
+  add = () =>{
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () =>{
+    this.setState({count: this.state.count - 1});
+  };
+  render(){
+    return (
       <div>
-        <h2>I like {name} </h2>
-        <img src={picture} alt="this is animals"/>
+        <h1>This number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
-    </div>);
-}
-
-Animals.propTypes = {
-  name:PropTypes.string.isRequired,
-  picture:PropTypes.string.isRequired
-};
-
-function renderAnimals(KindOfAnimals){
-  return <Animals key={KindOfAnimals.id} name={KindOfAnimals.name} picture={KindOfAnimals.image}/>
-}
-
-function App() {
-  return (
-    <div>
-      {KindOfAnimals.map(renderAnimals)}
-    </div>
-  );
+    )}
 }
 
 export default App;
